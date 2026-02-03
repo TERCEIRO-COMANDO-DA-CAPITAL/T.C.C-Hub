7d7572b9798a07c3c230367d8e08f62db008f5a6
TERCEIRO-COMANDO-DA-CAPITAL 🇪🇬😈🫶 <197555340+TERCEIRO-COMANDO-DA-CAPITAL@users.noreply.github.com>
2026-02-03 13:29:36 -0300

    Create NickChanger.lua

---
+++ Brookhaven/NickChanger.lua
@@ -0,0 +1,6 @@
+return function(NovoNick)
+        local args = {
+	"RolePlayName",
+	NovoNick}
+game:GetService("ReplicatedStorage"):WaitForChild("RE"):WaitForChild("1RPNam1eTex1t"):FireServer(unpack(args))
+end
