ddd700f837bdc8c3175e6bf3c4df3aee6899eb22
TERCEIRO-COMANDO-DA-CAPITAL 🇪🇬😈🫶 <197555340+TERCEIRO-COMANDO-DA-CAPITAL@users.noreply.github.com>
2026-02-03 13:43:21 -0300

    Update ColorNameChanger.lua

--- Brookhaven/ColorNameChanger.lua
+++ Brookhaven/ColorNameChanger.lua
@@ -1,7 +1,7 @@
-return function(cor)
+return function(r,g,b)
         local args = {
 	"PickingRPNameColor",
-	Color3.new(cor)
+	Color3.new(r,g,b)
 }
 game:GetService("ReplicatedStorage"):WaitForChild("RE"):WaitForChild("1RPNam1eColo1r"):FireServer(unpack(args))
 end
